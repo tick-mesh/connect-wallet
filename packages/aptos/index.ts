07f463db31f640e85350882ce07732de478a6aee
lybenson <naivebenson@gmail.com>
2022-11-02 19:15:22 +0800

    feat: update

--- packages/aptos/index.ts
+++ packages/aptos/index.ts
@@ -1,6 +1,24 @@
-// import { AptosClient } from 'aptos'
+import { AptosClient } from 'aptos'
 
-// class AptosClient {
+export default class Aptos {
+  client: AptosClient | null = null
+  constructor (url: string) {
+    this.client = new AptosClient(url)
+  }
 
-// }
-export {}
+  // createAccount () {
+  //   return this.client.
+  // }
+
+  // getAccount (): Promise<Types.AccountData> {
+  //   return this.client?.getAccount()
+  // }
+
+  // connect (): Promise<> {
+  //   return this.client.connect()
+  // }
+}
+
+const aptos = new Aptos('https://fullnode.devnet.aptoslabs.com/v1')
+
+console.log(aptos)
